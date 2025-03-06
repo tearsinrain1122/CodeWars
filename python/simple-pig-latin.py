9b598f1d24abd4df06d494725b46613dbc159c0f
tearsinrain1122 <tearsinrain1122@outlook.com>
2025-03-06 14:32:10 +0000

    Create simple-pig-latin.py

---
+++ python/simple-pig-latin.py
@@ -0,0 +1,11 @@
+# Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched
+
+def pig_it(text):
+    text = text.split()
+    pig_latin = []
+    for word in text:
+        if word not in [".", ",", "!", "-", "?"]:
+            pig_latin.append(word[1:]+word[0]+"ay")
+        else:
+            pig_latin.append(word)
+    return " ".join(pig_latin)
