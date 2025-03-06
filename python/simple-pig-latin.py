cddd63cac1aeacb865ea4a68d9efd11253165f1d
tearsinrain1122 <tearsinrain1122@outlook.com>
2025-03-06 14:55:01 +0000

    Keeping formatting consistent

--- python/simple-pig-latin.py
+++ python/simple-pig-latin.py
@@ -1,5 +1,7 @@
 # Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched
 
+# Solution
+
 def pig_it(text):
     text = text.split()
     pig_latin = []
